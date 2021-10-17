1a5d20a1e422be7439684097fa444450c47f5e24
vssakovich <valessakovich@gmail.com>
2021-10-17 22:43:24 +0300

    Initial Commit

---
+++ СаковичВалерия_СоцСеть/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  СаковичВалерия_СоцСеть
+//
+//  Created by Валерия Сакович on 17.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
